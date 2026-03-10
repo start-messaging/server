58f82e08df924f5e329596d20ea01275587b312c
vickydarlinn <uttapalsangwan@gmail.com>
2026-03-11 00:20:29 +0530

    feat:implement cors origins

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -44,6 +44,8 @@ export default () => ({
     origins: process.env.CORS_ORIGINS
       ? process.env.CORS_ORIGINS.split(',').map((s) => s.trim())
       : [
+          'https://app.startmessaging.com',
+          'https://admin.startmessaging.com',
           'http://localhost:5173',
           'http://localhost:5174',
           'http://localhost:5175',
